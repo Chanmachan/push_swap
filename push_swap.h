c199491a0f03621496fda560403ee180a098b105
chanma <yunosuke.homma@icloud.com>
2022-08-15 00:37:29 +0900

    rra rrb rrr

--- push_swap.h
+++ push_swap.h
@@ -13,10 +13,20 @@ typedef struct s_stack
 	int count_b;
 } t_stack;
 
+//swap
+void	sa(t_stack *stack);
+void	sb(t_stack *stack);
+void	ss(t_stack *stack);
+//rotate
 void	ra(t_stack *stack);
 void	rb(t_stack *stack);
 void	rr(t_stack *stack);
+//push
 void	pa(t_stack *stack);
 void	pb(t_stack *stack);
+//reverse_rotate
+void	rra(t_stack *stack);
+void	rrb(t_stack *stack);
+void	rrr(t_stack *stack);
 
 #endif
