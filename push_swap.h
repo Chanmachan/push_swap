941c8123837b87ab91c5800e524d60f8eac09175
chanma <yunosuke.homma@icloud.com>
2022-08-14 23:54:40 +0900

    t_stack ra rb rr

--- push_swap.h
+++ push_swap.h
@@ -4,4 +4,17 @@
 #include "libft/libft.h"
 #include "ft_printf/ft_printf.h"
 
+typedef struct s_stack
+{
+	int args;
+	int	*a;
+	int	*b;
+	int count_a;
+	int count_b;
+} t_stack;
+
+void	ra(t_stack *stack)
+void	rb(t_stack *stack)
+void	rr(t_stack *stack)
+
 #endif
