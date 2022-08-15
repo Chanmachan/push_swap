656cfef7172190bdd86d0007a38acc0175e8f5e4
chanma <yunosuke.homma@icloud.com>
2022-08-15 13:05:34 +0900

    change main

--- main.c
+++ main.c
@@ -1,19 +1,32 @@
 #include "push_swap.h"
-#include <stdio.h>
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	size_t	i;
-	i = 1;
-	if (argc > 1)
+	t_stack	stack;
+	size_t	n;
+	size_t	tmp;
+
+	tmp = argc;
+	set_start(&stack, argc);
+	init_argv(argv, &stack);
+	n = 0;
+	printf("***start***\n");
+	while (--tmp)
 	{
-		while (--argc > 0)
-		{
-			ft_printf("%s\n", argv[i]);
-			i++;
-		}
+		ft_printf("%d | %d\n", stack.a[n], stack.b[n]);
+		n++;
 	}
-	else
-		ft_printf("Error\n");
+	printf("-----------\n");
+	pb(&stack);
+	n = 0;
+	tmp = argc;
+	while (--tmp)
+	{
+		ft_printf("%d | %d\n", stack.a[n], stack.b[n]);
+		n++;
+	}
+	free(stack.a);
+	free(stack.b);
+//	system("leaks -q a.out");
 	return (0);
 }
