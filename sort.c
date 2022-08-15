a7820589af138312176880c556f4a9d05a725f49
chanma <yunosuke.homma@icloud.com>
2022-08-15 20:20:55 +0900

    adjust args == 3

--- sort.c
+++ sort.c
@@ -7,18 +7,34 @@ void	if_args_two(t_stack *stack)
 
 //場合わけ
 /*
- 1 2 3
- 1 3 2
- 2 1 3
- 2 3 1
- 3 1 2
- 3 2 1
+ 1 2 3 //sortedで完了済み
+ 1 3 2 rra sa
+ 2 1 3 sa
+ 2 3 1 rra
+ 3 1 2 ra
+ 3 2 1 sa rra
  */
 
-//void	if_args_three(t_stack *stack)
-//{
-//
-//}
+void	if_args_three(t_stack *stack)
+{
+	if (stack->a[0] < stack->a[2] && stack->a[2] < stack->a[1])
+	{
+		rra(stack);
+		sa(stack);
+	}
+	else if (stack->a[1] < stack->a[0] && stack->a[0] < stack->a[2])
+		sa(stack);
+	else if (stack->a[3] < stack->a[0] && stack->a[0] < stack->a[1])
+		rra(stack);
+	else if (stack->a[1] < stack->a[2] && stack->a[2] < stack->a[0])
+		ra(stack);
+	else if (stack->a[2] < stack->a[1] && stack->a[1] < stack->a[0])
+	{
+		sa(stack);
+		rra(stack);
+	}
+	return;
+}
 
 //基数ソートだと要素が２個とか３個のときに例外的に処理しないといけない
 void	check_args(t_stack *stack)
