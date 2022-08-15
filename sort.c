a76e4717b90252fb78ef1b2722737e311ad94a4e
chanma <yunosuke.homma@icloud.com>
2022-08-15 19:48:17 +0900

    add check_args func

--- sort.c
+++ sort.c
@@ -1,20 +1,52 @@
 #include "push_swap.h"
 
-//まずそもそもソートされてないかをチェックする
+void	if_args_two(t_stack *stack)
+{
+	sa(stack);
+}
+
+//場合わけ
+/*
+ 1 2 3
+ 1 3 2
+ 2 1 3
+ 2 3 1
+ 3 1 2
+ 3 2 1
+ */
+
+//void	if_args_three(t_stack *stack)
+//{
+//
+//}
 
 //基数ソートだと要素が２個とか３個のときに例外的に処理しないといけない
+void	check_args(t_stack *stack)
+{
+	if (stack->args == 2)
+		if_args_two(stack);
+	if (stack->args == 3)
+		if_args_three(stack);
+}
 
 //バブルソートで座標圧縮をする
 
+
 //座標圧縮をして二進数表記する
+
 
 //二進数の一桁目が'1'だった場合raで下に持っていく
 
+
 //'0'がきたらpb
+
 
 //分け終わったらpaで元に戻す
 
+
 //次の桁に進んで'0'だったらpb
 
+
 //'1'だったらraを繰り返す
+
 
