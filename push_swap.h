f9ce65def6fa293fe652b7375a04b7186102ac6e
chanma <yunosuke.homma@icloud.com>
2022-08-19 03:25:23 +0900

    adjust norm

--- push_swap.h
+++ push_swap.h
@@ -1,19 +1,19 @@
-#ifndef PUSH_SWAP_PUSH_SWAP_H
-#define PUSH_SWAP_PUSH_SWAP_H
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 
-#include "libft/libft.h"
-#include "ft_printf/ft_printf.h"
-#include <stdio.h>
+# include "libft/libft.h"
+# include "ft_printf/ft_printf.h"
+# include <stdio.h>
 
 typedef struct s_stack
 {
-	int args;
+	int	args;
 	int	*a;
 	int	*b;
 	int	*dup_a;
-	int count_a;
-	int count_b;
-} t_stack;
+	int	count_a;
+	int	count_b;
+}				t_stack;
 
 //swap
 void	sa(t_stack *stack);
@@ -31,14 +31,15 @@ void	rra(t_stack *stack);
 void	rrb(t_stack *stack);
 void	rrr(t_stack *stack);
 //sorted
-int	sorted(t_stack *stack);
+int		sorted(t_stack *stack);
 //sort
 void	sort_args(t_stack *stack);
 //coordinate_compression
 void	ccompression(t_stack *stack);
 //atoi_plus
-int atoi_plus(const char *str, t_stack *stack);
+int		atoi_plus(const char *str, t_stack *stack);
 //exit_fail
 void	exit_fail(t_stack *stack);
+void	exit_success(t_stack *stack);
 
 #endif
