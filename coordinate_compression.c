35dfdaa09eeefeb3a8669bd0b1476b8c4fe88dde
chanma <yunosuke.homma@icloud.com>
2022-08-19 03:07:48 +0900

    add free && adjust norm

--- coordinate_compression.c
+++ coordinate_compression.c
@@ -1,7 +1,5 @@
 #include "push_swap.h"
 
-//座標圧縮する関数
-
 void	ccompression(t_stack *stack)
 {
 	size_t	i;
@@ -24,23 +22,3 @@ void	ccompression(t_stack *stack)
 	}
 	free(stack->dup_a);
 }
-
-//int main(void)
-//{
-//	t_stack stack;
-//	stack.args = 3;
-//	size_t i = 0;
-//	while (i < (size_t)stack.args)
-//	{
-//		printf("%d\n", (ccompression(&stack))[i]);
-//		i++;
-//	}
-//}
-
-/*
-12 34 5 66 54 1
- 3  4 2  6  5 1
-
-1 2  3  4  5  6
-1 5 12 34 54 66
-*/
