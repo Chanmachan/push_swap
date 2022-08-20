106f3bf7bc39340b76b14646ccf5fe20dea4c073
chanma <yunosuke.homma@icloud.com>
2022-08-21 04:22:57 +0900

    re

--- srcs/atoi_plus.c
+++ srcs/atoi_plus.c
@@ -1,15 +1,5 @@
 #include "push_swap.h"
 
-int	check_zero(char *str)
-{
-	if (str[0] == '0' && ft_strlen(str) > 1)
-	{
-		ft_printf("Error\n");
-		exit(EXIT_FAILURE);
-	}
-	return (0);
-}
-
 static int	ft_isspace(int c)
 {
 	if (c == ' ' || c == '\n' || c == '\r' || \
@@ -35,11 +25,13 @@ int	atoi_plus(const char *str, t_stack *stack)
 			sign = -1;
 		i++;
 	}
-	check_zero((char *)str);
 	if (!ft_isdigit(str[i]))
 		exit_fail(stack);
-	while (ft_isdigit(str[i++]))
+	while (ft_isdigit(str[i]))
+	{
 		ret = (ret * 10) + sign * (str[i] - '0');
+		i++;
+	}
 	if (ret > INT_MAX || ret < INT_MIN)
 		exit_fail(stack);
 	return ((int)ret);
