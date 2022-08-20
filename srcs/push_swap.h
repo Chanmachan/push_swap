d736b9e12a96f51507135aca921a105795d02c23
chanma <yunosuke.homma@icloud.com>
2022-08-21 01:49:28 +0900

    change directory

--- srcs/push_swap.h
+++ srcs/push_swap.h
@@ -1,8 +1,8 @@
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# include "libft/libft.h"
-# include "ft_printf/ft_printf.h"
+# include "../libft/libft.h"
+# include "../ft_printf/ft_printf.h"
 # include <stdio.h>
 
 typedef struct s_stack
